74dc6197586486a8fd90829276e76f1246b765e9
Charlie Chen <charlie@jomo.network>
2024-02-19 15:23:26 -0800

    Update to latest tlsn

--- wasm_verifier_lib/pkg/wasm_verifier_lib_bg.wasm.d.ts
+++ wasm_verifier_lib/pkg/wasm_verifier_lib_bg.wasm.d.ts
@@ -2,7 +2,7 @@
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
 export function verifyProofs(a: number, b: number, c: number, d: number, e: number, f: number): void;
-export function ring_core_0_17_7_bn_mul_mont(a: number, b: number, c: number, d: number, e: number, f: number): void;
+export function ring_core_0_17_8_bn_mul_mont(a: number, b: number, c: number, d: number, e: number, f: number): void;
 export function __wbindgen_add_to_stack_pointer(a: number): number;
 export function __wbindgen_malloc(a: number, b: number): number;
 export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
